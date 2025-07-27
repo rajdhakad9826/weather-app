0894adbeba7ada97b82d50a9bcc1e5caaff1ff8d
Raj Dhakad <officialrajdhakad@gmail.com>
2025-07-27 22:45:00 +0530

    set up OpenWeatherMap API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { use, useEffect } from 'react'
 import './App.css'
+const apiKey = import.meta.env.VITE_API_KEY;
+console.log('Loaded API Key:', import.meta.env.VITE_API_KEY);
 
 function App() {
+
+  useEffect(() => {
+    fetchWeatherData()  
+  }, [])
+
+  const fetchWeatherData = async () => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${apiKey}`)
+    const data = await response.json()
+  }
 
   return (
     <>
