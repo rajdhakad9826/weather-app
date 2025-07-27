0ad381a3e1396d0d0f9f4298e6e0ae21b96b7b5b
Raj Dhakad <officialrajdhakad@gmail.com>
2025-07-27 23:52:03 +0530

    complete SearchBar component with styling and debounced API integration

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = ({ city, setCity }) => {
   return (
-    <div>SearchBar</div>
+    <div className="searchContainer">
+      <input type="text" className="searchBar" name="search" placeholder="search for a city..." id="search" value={city} onChange={(e) => setCity(e.target.value)}></input>
+    </div>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { use, useEffect } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
+import SearchBar from '../components/SearchBar';
 const apiKey = import.meta.env.VITE_API_KEY;
-console.log('Loaded API Key:', import.meta.env.VITE_API_KEY);
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null)
+  const [city, setCity] = useState('Delhi')
 
   useEffect(() => {
-    fetchWeatherData()  
-  }, [])
+    if (!city) return;
+    const timer = setTimeout(() => {
+      fetchWeatherData()
+    }, 800) 
+    return () => clearTimeout(timer)
+  }, [city])
 
   const fetchWeatherData = async () => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${apiKey}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     const data = await response.json()
+    console.log(`Fetching weather data for ${city}...`)
+    console.log(data)
+    if (data.cod !== 200) {
+      console.error(`Error fetching weather data: ${data.message}`);
+      return;
+    }
+    if (data.cod === '404') {
+      console.error('City not found');
+      return;
+    }
+    setWeatherData(data);
   }
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div className="app">
+      <SearchBar city={city} setCity={setCity} />
+      <h1>{weatherData ? weatherData.sys.country : 'Loading...'}</h1>
+    </div>
   )
 }
 
