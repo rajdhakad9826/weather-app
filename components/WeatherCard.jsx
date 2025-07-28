203b52d01e21e1d2da44f5981e7981ea24bc6d55
Raj Dhakad <officialrajdhakad@gmail.com>
2025-07-28 22:03:37 +0530

    add weather icons with react-icons

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -1,33 +1,67 @@
 import React from "react";
 import "./WeatherCard.css";
+import { IoIosSunny } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+
+import {
+  WiDaySunny,
+  WiCloud,
+  WiRain,
+  WiThunderstorm,
+  WiSnow,
+  WiFog,
+  WiSprinkle,
+  WiDust,
+  WiTornado,
+} from "react-icons/wi";
+
+const getWeatherIcon = (main) => {
+  switch (main) {
+    case "Clear":
+      return <WiDaySunny/>;
+    case "Clouds":
+      return <WiCloud/>;
+    case "Rain":
+      return <WiRain/>;
+    case "Drizzle":
+      return <WiSprinkle/>;
+    case "Thunderstorm":
+      return <WiThunderstorm/>;
+    case "Snow":
+      return <WiSnow/>;
+    case "Mist":
+    case "Haze":
+    case "Fog":
+      return <WiFog/>;
+    case "Dust":
+    case "Sand":
+      return <WiDust/>;
+    case "Tornado":
+      return <WiTornado/>;
+    default:
+      return <WiDaySunny/>;
+  }
+};
 
 const WeatherCard = ({ weatherData }) => {
   return (
     <div className="weatherCard myevent">
       <div className="location">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="#e3e3e3"
-        >
-          <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
-        </svg>
+        <FaLocationDot />
         <span>
-          {weatherData?.name}, {weatherData?.sys?.country}
+          {weatherData?.name ?? 'Unknown City'}, {weatherData?.sys?.country ?? 'Unknown Country'}
         </span>
       </div>
       <div className="weather">
         <div className="temperature">
-          <h1>{Math.round(weatherData?.main?.temp)}&deg;</h1>
-          <p className="description">{weatherData?.weather[0]?.description}</p>
+          <h1>{Math.round(weatherData?.main?.temp ?? 0)}&deg;</h1>
+          <p className="description">{weatherData?.weather[0]?.description ?? 'Unknown Weather'}</p>
           <p className="feels-like">
-            Feels like {Math.round(weatherData?.main?.feels_like)}&deg;
+            Feels like {Math.round(weatherData?.main?.feels_like ?? 0)}&deg;
           </p>
         </div>
         <div className="icon">
-         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm283-100q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q66 0 113-47t47-113q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-160Z"/></svg>
+          {getWeatherIcon(weatherData?.weather[0]?.main)}
         </div>
       </div>
     </div>
